2c9d6ea7e0f5c1e7b787b3698b5a4b82d37e6540
Yosuke ONOUE <onoue@likr-lab.com>
2015-05-12 17:19:13 +0900

    Change API

--- d3-downloadable.d.ts
+++ d3-downloadable.d.ts
@@ -1,23 +1,11 @@
 /// <reference path="../d3/d3.d.ts"/>
 
-declare module D3 {
-  interface Downloadable {
-    (selection: D3.Selection): void;
-    filename: {
-      (): string;
-      (arg: string): Downloadable;
-    };
-    width: {
-      (): number;
-      (arg: number): Downloadable;
-    };
-    height: {
-      (): number;
-      (arg: number): Downloadable;
-    };
-  }
-
-  interface Base extends Selectors {
-    downloadable(arg: any): Downloadable;
-  }
+interface Downloadable {
+  (selection: D3.Selection): void;
+  filename: {
+    (): string;
+    (arg: string): Downloadable;
+  };
 }
+
+declare function downloadable(): Downloadable;
